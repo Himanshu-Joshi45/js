f0613250846e697053e912e373eb1476d32e1dbd
Himanshu Joshi <160339968+Himanshu-Joshi45@users.noreply.github.com>
2024-03-09 08:27:22 +0000

    done

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 1444;
+let accountEmail = "himanshu@gemail.com";
+var accountPassword = "123";
+
+//accountId = 23 // not allowed
+accountEmail = "hc@gmail.com"
+accountPassword = "12233"
+console.log(accountEmail);
+console.log(accountId);
+console.log(accountPassword);
