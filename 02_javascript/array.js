15cb4dffa21f7b93dffa8000b71050fdbd601104
Himanshu Joshi <160339968+Himanshu-Joshi45@users.noreply.github.com>
2024-03-10 09:36:29 +0000

    array config done

---
+++ 02_javascript/array.js
@@ -0,0 +1,9 @@
+const myArr = [10,2,3,4,5,3]
+
+const myArr2 = new Array(3,5,432,2)
+console.log(myArr[0])
+console.log(myArr2[2])
+myArr.push(78)
+console.log(myArr)
+myArr.pop()
+console.log(myArr)
